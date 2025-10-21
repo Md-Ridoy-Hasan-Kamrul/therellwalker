f40f62425a5be63b3e191706b85cae5f3044b1da
Md. Ridoy Hasan Kamrul <80632331+Md-Ridoy-Hasan-Kamrul@users.noreply.github.com>
2025-10-21 11:46:10 +0600

    refactor: update Sidebar component icons and improve styling

--- src/components/common/Sidebar.jsx
+++ src/components/common/Sidebar.jsx
@@ -1,31 +1,29 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import {
-  IoGridOutline,
-  IoAddCircleOutline,
-  IoListOutline,
-  IoCreateOutline,
-} from 'react-icons/io5';
+import { AiOutlinePieChart } from 'react-icons/ai';
+import { RiBarChartLine } from 'react-icons/ri';
+import { FaChartLine } from 'react-icons/fa6';
+import { BsArrowsExpandVertical } from 'react-icons/bs';
 
 const navLinks = [
-  { to: '/', text: 'Dashboard', icon: IoGridOutline },
-  { to: '/trade-entry', text: 'Trade Entry', icon: IoAddCircleOutline },
-  { to: '/trade-log', text: 'Trade Log', icon: IoListOutline },
-  { to: '/reflections', text: 'Reflections', icon: IoCreateOutline },
+  { to: '/', text: 'Dashboard', icon: AiOutlinePieChart },
+  { to: '/trade-entry', text: 'Trade Entry', icon: RiBarChartLine },
+  { to: '/trade-log', text: 'Trade Log', icon: FaChartLine },
+  { to: '/reflections', text: 'Reflections', icon: BsArrowsExpandVertical },
 ];
 
 export const Sidebar = () => {
   const baseStyle =
-    'w-full h-16 px-6 py-4 rounded-lg flex items-center gap-4 transition-all duration-200';
+    'w-full px-6 py-4 rounded-lg flex items-center gap-6 transition-all duration-200 h-16';
 
   const activeStyle =
-    'bg-gradient-to-l from-gray-800 via-fuchsia-800 to-pink-500 shadow-lg text-white font-semibold';
+    'bg-gradient-to-l from-gray-800 via-fuchsia-800 to-pink-500 shadow-[0px_20px_50px_0px_rgba(55,69,87,0.10)] text-white font-semibold';
 
-  const inactiveStyle = 'text-zinc-400 font-normal hover:bg-neutral-700';
+  const inactiveStyle = 'text-zinc-400 font-normal hover:bg-neutral-700/50';
 
   return (
-    <aside className='hidden md:flex w-80 h-full bg-neutral-800 px-6 py-12 flex-col'>
-      <nav className='flex flex-col items-center gap-6'>
+    <aside className='hidden md:flex w-80 h-screen bg-neutral-800 px-6 py-8 flex-col overflow-hidden'>
+      <nav className='flex flex-col items-center gap-4 mt-4'>
         {navLinks.map((link) => {
           const Icon = link.icon;
           return (
