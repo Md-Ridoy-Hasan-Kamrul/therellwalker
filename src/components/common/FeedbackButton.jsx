88e8874e14462cf93cd39c57e212e607990c337c
Md. Ridoy Hasan Kamrul <80632331+Md-Ridoy-Hasan-Kamrul@users.noreply.github.com>
2025-10-22 15:43:32 +0600

    refactor: add click handler to FeedbackButton for external link navigation

--- src/components/common/FeedbackButton.jsx
+++ src/components/common/FeedbackButton.jsx
@@ -2,9 +2,16 @@ import React from 'react';
 import { BsFillChatLeftDotsFill } from 'react-icons/bs';
 
 const FeedbackButton = () => {
+  const handleClick = () => {
+    window.open('https://www.facebook.com', '_blank');
+  };
+
   return (
     <div className='fixed right-0 bottom-20 z-50'>
-      <div className='w-12 h-44 bg-gradient-to-b from-[#924a8f] to-[#5d3658] rounded-tl-3xl rounded-bl-3xl shadow-xl flex flex-col items-center justify-between py-5 cursor-pointer hover:opacity-90 transition-opacity'>
+      <div 
+        onClick={handleClick}
+        className='w-12 h-44 bg-gradient-to-b from-[#924a8f] to-[#5d3658] rounded-tl-3xl rounded-bl-3xl shadow-xl flex flex-col items-center justify-between py-5 cursor-pointer hover:opacity-90 transition-opacity'
+      >
         {/* Feedback Text - Vertical (rotated 90 degrees counter-clockwise) */}
         <div className='flex-1 flex items-center justify-center'>
           <span
